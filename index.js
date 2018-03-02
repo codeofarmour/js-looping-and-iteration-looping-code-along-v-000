0d079f8d32d755e5d7bc45998e2fda149b9e1d2b
Terri Riggle <codeofarmour@outlook.com>
2018-03-02 19:34:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function printBadges (names) {
   for (let i = 0; i < names.length; i++) {
     console.log(`Welcome ${names[i]}!' You are employee #${i + 1}.`);
   }
+  
   return names;
 }
 
@@ -11,5 +12,6 @@ function tailsNeverFails () {
   while (Math.random() >= 0.5) {
     tails++;
   }
+  
   return `You got ${tails} tails in a row!`;
 }
